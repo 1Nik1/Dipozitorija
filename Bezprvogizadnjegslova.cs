714d26cbb7c25164a076d25ca30ab4141361935c
Nikola Bugarin <69224309+1Nik1@users.noreply.github.com>
2024-03-15 10:08:16 +0100

    Add files via upload

---
+++ Bezprvogizadnjegslova.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class KlasaB
+{
+    public string BezPrvogIZadnjeg(string tekst)
+    {
+        if (tekst.Length <= 2)
+        {
+            Console.WriteLine("Tekst mora imati više od 2 znaka jer inače nema što ispisati!!!!!!!!!!!!!!!!!!!!!!");
+            return "";
+        }
+
+        return tekst.Substring(1, tekst.Length - 2);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        KlasaB objekt = new KlasaB();
+
+        Console.WriteLine("Unesite riječ:");
+        string unos = Console.ReadLine();
+
+        if (unos.Length > 2)
+        {
+            string rezultat = objekt.BezPrvogIZadnjeg(unos);
+            Console.WriteLine("Riječ bez prvog i zadnjeg slova: " + rezultat);
+        }
+        else
+        {
+            Console.WriteLine("Tekst mora imati više od 2 znaka jer inače nema što ispisati!!!!!!!!!!!!!!!!!!!!!!");
+        }
+
+        Console.ReadLine(); // Zaustavi konzolu da se zatvori odmah
+    }
+}

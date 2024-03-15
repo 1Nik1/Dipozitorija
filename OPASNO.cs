b406eeb0c2bf26c17099e89abdc5b0e53b45af08
Nikola Bugarin <69224309+1Nik1@users.noreply.github.com>
2024-03-15 11:15:49 +0100

    Add files via upload

---
+++ OPASNO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Cat
+{
+    private bool opasna;
+
+    public bool Opasna { get => opasna; set => opasna = value; }
+
+    public void Opasnost()
+    {
+        Opasna = false;
+    }
+}
+
+class Gepard : Cat
+{
+    public new void Opasnost()
+    {
+        base.Opasnost();
+        Opasna = true;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Gepard g = new Gepard();
+        g.Opasnost();
+
+        Console.WriteLine("Vrijednost opasna u objektu g: " + g.Opasna);
+
+        Console.ReadLine();
+    }
+}
